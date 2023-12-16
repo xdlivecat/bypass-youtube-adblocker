c83fa3350cdd4a327d59ad48abadaeb2f6abf254
xdlivecat <107507678+xdlivecat@users.noreply.github.com>
2023-12-16 17:07:48 +0100

    Add files via upload

---
+++ content.js
@@ -0,0 +1,16 @@
+function removeDialogAndResumeVideo() {
+    var elementsToRemove = document.querySelectorAll('tp-yt-paper-dialog[style-target="host"][role="dialog"][tabindex="-1"][style*="position: fixed; top: 242.25px; left: 270px;"]');
+
+    for (var i = 0; i < elementsToRemove.length; i++) {
+        elementsToRemove[i].remove();
+    }
+
+    var playButton = document.querySelector('button.ytp-play-button');
+    if (playButton) {
+        playButton.click();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    removeDialogAndResumeVideo();
+});
